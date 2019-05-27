2dc755304f9b8e3d6c8c4bcdeec2c7bb17c19525
Sundara Subramanian <48820070+surya0598@users.noreply.github.com>
2019-05-27 23:46:37 +0530

    Create vowelconsonant.py

---
+++ vowelconsonant.py
@@ -0,0 +1,7 @@
+x=str(input())
+if(x=="a" or x=="e" or x=="i" or x=="o" or x=="u"):
+  print("Vowel")
+elif (x=="b" or x=="c" or x=="d" or x=="f" or x=="g" or x=="h" or x=="j" or x=="k" or x=="l" or x=="m" or x=="n" or x=="p" or x=="q" or x=="r" or x=="s" or x=="t" or x=="v" or x=="w" or x=="x" or x=="y" or x=="z"):
+  print("Consonant")
+else:
+  print("invalid")

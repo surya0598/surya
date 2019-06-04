b1b2c7c25c562a4343920c1602db317c396d0762
Sundara Subramanian <48820070+surya0598@users.noreply.github.com>
2019-06-04 16:00:49 +0530

    Update largestofnum.py

--- largestofnum.py
+++ largestofnum.py
@@ -1,2 +1,10 @@
-a=list(map(int,input().split()))
-print(max(a))
+x=list(map(int,input().split()))
+a=x[0]
+b=x[1]
+c=x[2]
+if a>b and a>c:
+  print(a)
+elif b>a and b>c:
+  print(b)
+else:
+  print(c)
